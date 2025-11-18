f894515ed7df2024aa769d4c7b769796306d3b8f
thedevlake <sofia.devx@gmail.com>
2025-11-18 15:05:27 +0100

    fix: use wget instead of curl for health check

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -45,7 +45,7 @@ resource "aws_ecs_task_definition" "backend" {
       ]
 
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:8080/api/v1/health || exit 1"]
+        command     = ["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost:8080/api/v1/health || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
